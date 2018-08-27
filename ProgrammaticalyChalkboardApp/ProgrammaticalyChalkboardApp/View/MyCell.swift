3aefbe79b70cf8ea93617f03591e07567eadefd6
Israel Manzo <israelmanzo814@gmail.com>
2018-08-27 13:32:05 -0400

    Updating table view cell files

---
+++ ProgrammaticalyChalkboardApp/ProgrammaticalyChalkboardApp/View/MyCell.swift
@@ -0,0 +1,37 @@
+//
+//  MyCell.swift
+//  ProgrammaticalyChalkboardApp
+//
+//  Created by Israel Manzo on 8/27/18.
+//  Copyright © 2018 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+    
+//    var list: 
+    
+    let cellLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCellView()
+    }
+    
+    func setCellView(){
+        addSubview(cellLabel)
+        
+        cellLabel.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
