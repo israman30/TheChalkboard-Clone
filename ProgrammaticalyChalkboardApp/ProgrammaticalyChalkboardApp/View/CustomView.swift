686d9a00f1469d39cf6cedd5ae1ccf6603eee1fb
Israel Manzo <israelmanzo814@gmail.com>
2020-06-07 21:44:05 -0400

    Updated the chalboard

---
+++ ProgrammaticalyChalkboardApp/ProgrammaticalyChalkboardApp/View/CustomView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomView.swift
+//  ProgrammaticalyChalkboardApp
+//
+//  Created by Israel Manzo on 6/7/20.
+//  Copyright © 2020 Israel Manzo. All rights reserved.
+//
+
+import Foundation
