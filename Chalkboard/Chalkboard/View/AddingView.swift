e3e2bc499feb9481e56ac182d2e2f3b1ab9d230d
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-03-14 18:36:21 -0400

    created view model file

--- Chalkboard/Chalkboard/View/AddingView.swift
+++ Chalkboard/Chalkboard/View/AddingView.swift
@@ -6,50 +6,6 @@
 //
 
 import SwiftUI
-import CoreData
-
-struct NewItem {
-    let item: String = ""
-    let date = Date()
-}
-
-class ItemViewModel: ObservableObject {
-    var context: NSManagedObjectContext?
-    private let newItem = NewItem()
-    @Published var addedItem = ""
-    @Published var date = Date()
-    
-    var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .full
-        formatter.timeStyle = .short
-        return formatter
-    }
-    
-    init(context: NSManagedObjectContext? = nil) {
-        self.context = context
-    }
-    
-    func add() {
-        let newTask = Task(context: context!)
-        newTask.id = UUID()
-        newTask.name = addedItem
-        newTask.timestamp = dateFormatter.string(from: date)
-        save()
-        close()
-    }
-    private func save() {
-        do {
-            try context!.save()
-        } catch {
-            print("Error saving task in db: \(error.localizedDescription)")
-        }
-    }
-    
-    func close() {
-        self.addedItem = ""
-    }
-}
 
 struct AddingView: View {
     
---
+++ Chalkboard/Chalkboard/ViewModel/ItemViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  ItemViewModel.swift
+//  Chalkboard
+//
+//  Created by Israel Manzo on 3/14/23.
+//
+
+import Foundation
+import CoreData
+
+class ItemViewModel: ObservableObject {
+    
+    var context: NSManagedObjectContext?
+    @Published var addedItem = ""
+    @Published var date = Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
+    init(context: NSManagedObjectContext? = nil) {
+        self.context = context
+    }
+    
+    func add() {
+        let newTask = Task(context: context!)
+        newTask.id = UUID()
+        newTask.name = addedItem
+        newTask.timestamp = dateFormatter.string(from: date)
+        save()
+        close()
+    }
+    private func save() {
+        do {
+            try context!.save()
+        } catch {
+            print("Error saving task in db: \(error.localizedDescription)")
+        }
+    }
+    
+    func close() {
+        self.addedItem = ""
+    }
+}
